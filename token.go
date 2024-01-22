d8902eadbf86083425006aeb2377a30725338700
Cyber MacGeddon <cybermaggedon@gmail.com>
2024-01-22 21:55:21 +0000

    Working now?

--- token.go
+++ token.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 	"time"
 
-	"golang.org/x/oauth2/internal"
+	"github.com/kalntera/oauth2/internal"
 )
 
 // defaultExpiryDelta determines how earlier a token should be considered
